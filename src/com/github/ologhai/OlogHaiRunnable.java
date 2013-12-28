6952d114e958870da37600121d8e549da1be8a52
etsai <etsai2@gmail.com>
2013-12-27 18:24:03 -0800

    Implemented drawing a circle on the screen

---
+++ src/com/github/ologhai/OlogHaiRunnable.java
@@ -0,0 +1,45 @@
+package com.github.ologhai;
+
+import java.lang.Runnable;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+
+public class OlogHaiRunnable implements Runnable {
+
+	private final Context context;
+	private final SurfaceHolder holder;
+	private boolean running= true;
+	
+	public OlogHaiRunnable(Context context, SurfaceHolder holder) {
+		this.context= context;
+		this.holder= holder;
+	}
+	
+	@Override
+	public void run() {
+		Paint circlePaint= new Paint();
+		
+		circlePaint.setARGB(255, 0, 0, 255);
+		while(running) {
+			synchronized(holder) {
+				Canvas canvas= holder.lockCanvas();
+				if (canvas == null) continue;
+				
+				canvas.drawARGB(255, 255, 255, 255);
+				canvas.drawCircle(canvas.getWidth()/2, canvas.getHeight()/2, 20, circlePaint);
+			}
+			
+			try {
+				Thread.sleep(33);
+			} catch (Exception ex) {
+				//What do we do now?!
+			}
+		}
+	}
+	
+	public void setRunning(boolean running) {
+		this.running= running;
+	}
+}
--- src/com/github/ologhai/OlogHaiView.java
+++ src/com/github/ologhai/OlogHaiView.java
@@ -2,12 +2,36 @@ package com.github.ologhai;
 
 import android.content.Context;
 import android.view.SurfaceHolder;
-import android.view.View;
+import android.view.SurfaceView;
 
-public class OlogHaiView extends View implements SurfaceHolder.Callback {
+public class OlogHaiView extends SurfaceView implements SurfaceHolder.Callback {
 
+	private final Context context;
+	
 	public OlogHaiView(Context context) {
 		super(context);
+		
+		getHolder().addCallback(this);
+		setKeepScreenOn(true);
+		this.context= context;
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
